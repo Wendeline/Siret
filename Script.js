11a361323526adf7e7f203502ce8753728031819
Wendeline <wendeline.swart.lnd@gmail.com>
2021-08-29 17:19:23 +0200

    Formulaire

--- Script.js
+++ Script.js
@@ -1,3 +1,7 @@
 function Search() {
+    //Récupération des éléments de l'index
+    var siret = document.getElementById('SIRET').value;
+    var DivResult = document.getElementById('Result');
 
+    DivResult.innerHTML = siret;
 }
